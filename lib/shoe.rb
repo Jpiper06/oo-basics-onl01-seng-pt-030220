834158a8a54583dabbaa43e9ef1469e7484dcb13
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-23 23:58:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe 
+  def initiatlize(brand)
+    @brand = brand 
+  end 
+  
+  attr_reader :brand 
+  attr_accessor :color, :size, :material, :condition 
+  
+  def cobbleaf
+    self.conditin = "new"
+    puts "Your shoe is as god as new!"
+  end 
+end 
+
